3c72add1848e13c16423a72fb41707dd28e90241
NOouhan <145235972+NOouhan@users.noreply.github.com>
2024-11-28 16:52:45 +0200

    Add files via upload

---
+++ NurseViewModel .cs
@@ -0,0 +1,57 @@
+﻿
+//using System.ComponentModel.DataAnnotations;
+
+//namespace HospitalMS.ViewModels
+//{
+//    public class NurseViewModel
+//    {
+//        [Required]
+//        public int Id { get; set; }
+
+//        [Required]
+//        [Display(Name = "First Name")]
+//        public string FName { get; set; }
+
+//        [Required]
+//        [Display(Name = "Last Name")]
+//        public string LName { get; set; }
+
+//        [Required]
+//        [Display(Name = "Gender")]
+//        public string Gender { get; set; }
+
+//        [Display(Name = "Birth Date")]
+//        public DateOnly BirthDate { get; set; }
+
+//        [Required]
+//        [EmailAddress]
+//        [Display(Name = "Email")]
+//        public string Email { get; set; }
+
+//        [Required]
+//        [Phone]
+//        [Display(Name = "Phone")]
+//        public string Phone { get; set; }
+
+//        [Required]
+//        [Display(Name = "Username")]
+//        public string Username { get; set; }
+
+//        [Required]
+//        [Display(Name = "Password")]
+//        public string Password { get; set; }
+
+//        [Required]
+//        [Display(Name = "Salary")]
+//        public int Salary { get; set; }
+
+//        [Required]
+//        [Display(Name = "Speciality")]
+//        public string Speciality { get; set; }
+
+//        public string? Imag { get; set; }
+
+//        [Required]
+//        public int DepartmentId { get; set; }
+//    }
+//}
